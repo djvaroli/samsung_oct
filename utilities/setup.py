f8a8e587cef8c3a50b00a92314d8c3e848402746
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-13 23:05:20 -0400

    added setup file to custom utilities package

---
+++ utilities/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name="utilities",
+    version="0.0.1",
+    author="Daniel Varoli",
+    author_email="daniel.varoli@gmail.com",
+    description="Utility functions in Python for OCT Project",
+    long_description="Long description placeholder",
+    long_description_content_type="text/markdown",
+    url="https://github.com/prmsolutions/backend",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        'ciso8601',
+        'google-cloud-storage',
+        'numpy',
+        'simplejson'
+    ],
+    python_requires='>=3.8'
+)

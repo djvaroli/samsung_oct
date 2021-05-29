f4bdc00503d0cec77747a343827d09ddc204f56b
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-29 00:12:24 -0400

    style revamp and slight hack for updating page without reload

--- molo/app.py
+++ molo/app.py
@@ -1,13 +1,9 @@
 import os
-from PIL import Image
 
-import numpy as np
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
-
-from route_decorators import status_code_decorator
 import serving_utils
 import image_utils
 
@@ -19,37 +15,55 @@ app.config['UPLOAD_PATH'] = 'uploads'
 # MODEL_URI = "prediction-model:8500"
 MODEL_URI = "0.0.0.0:8500"
 
+CACHED_PREDICTIONS = {}
+
 
 @app.errorhandler(413)
 def too_large(e):
+    """
+
+    :param e:
+    :return:
+    """
     return "File is too large", 413
 
 
 @app.route("/", methods=['GET'])
 def home():
+    """
+
+    :return:
+    """
     files = os.listdir(app.config['UPLOAD_PATH'])
     return render_template("index.html", files=files)
 
 
-@app.route('/predict', methods=['POST'])
-def predict_image_class():
+@app.route('/predict/cache', methods=['POST'])
+def cache_predict_image_class():
+    """
+
+    :return:
+    """
     img = image_utils.uploaded_image_to_array(request.files['file'])
+    filename = secure_filename(request.files['file'].filename)
+
     model_input_shape = serving_utils.get_input_shape(MODEL_URI, model_name="dense_net")
     img = image_utils.resize_image(img, model_input_shape[:-1])
     img = image_utils.normalize_image(img)
     img = image_utils.make_image_into_batch(img)
     class_, prob_ = serving_utils.predict_ocular_myopathy_class(img, MODEL_URI, model_name="dense_net", timeout=3.0)
-    data = {
+    CACHED_PREDICTIONS[filename] = {
         "class": class_,
         "probability": prob_
     }
-    return data, 204
+    return '', 204
 
 
-@app.route("/molo/info/<model_name>", defaults={"model_name": "dense_net"}, methods=["GET"])
-@status_code_decorator
-def molo_info(
-        model_name: str
-):
-    metadata = serving_utils.get_model_metadata(model_uri=MODEL_URI, model_name=model_name)
-    return metadata
+@app.route('/predict/results', methods=['GET'])
+def get_cached_predictions():
+    """
+
+    :return:
+    """
+    print(CACHED_PREDICTIONS)
+    return CACHED_PREDICTIONS

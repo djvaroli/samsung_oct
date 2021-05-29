ab728eeb1cb82c78a1feba20f573496de97048c6
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-29 12:25:21 -0400

    bug fix in generator utils

--- utilities/utils/generator_utils.py
+++ utilities/utils/generator_utils.py
@@ -5,19 +5,46 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 def get_generator_instances(
-        **train_generator_kwargs
+        **kwargs
 ):
     """
     Returns instances of ImageDataGenerator to be used downstream for creating flows
     :param train_generator_kwargs:
     :return:
     """
-    generator_kwargs = dict(
-        preprocessing_function=lambda x: x / 255.
+
+    valid_generator_kwargs = dict(
+        featurewise_center=False,
+        samplewise_center=False,
+        featurewise_std_normalization=False,
+        samplewise_std_normalization=False,
+        zca_whitening=False,
+        zca_epsilon=1e-06,
+        rotation_range=0,
+        width_shift_range=0.0,
+        height_shift_range=0.0,
+        brightness_range=None,
+        shear_range=0.0,
+        zoom_range=0.0,
+        channel_shift_range=0.0,
+        fill_mode='nearest',
+        cval=0.0,
+        horizontal_flip=False,
+        vertical_flip=False,
+        rescale=None,
+        preprocessing_function=None,
+        data_format=None,
+        validation_split=0.0,
+        dtype=None
     )
 
-    generator = ImageDataGenerator(**generator_kwargs)
-    train_generator = ImageDataGenerator(**generator_kwargs, **train_generator_kwargs)
+    base_kwargs = dict(preprocessing_function=lambda x: x / 255.)
+
+    # this ensures only valid arguments are passed on and invalid ones don't throw and error
+    train_generator_kwargs = {k: v for k, v in kwargs.items() if valid_generator_kwargs.get(k)}
+
+    generator = ImageDataGenerator(**base_kwargs)
+    train_generator = ImageDataGenerator(**base_kwargs, **train_generator_kwargs)
 
     return train_generator, generator
 

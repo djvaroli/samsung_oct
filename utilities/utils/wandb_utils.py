34db5ed927e944ea787e15f3f993fb95f4664b34
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-29 12:19:14 -0400

    config as dict for sweeps

--- utilities/utils/wandb_utils.py
+++ utilities/utils/wandb_utils.py
@@ -9,5 +9,9 @@ def config_to_dict(config: wandb.config):
     :return:
     """
     d: Dict[str, Any] = config.__dict__
+
+    if "_items" in d:
+        d = d['_items']
+
     d = {k: v for k, v in d.items() if not (k.startswith("__") or k.endswith("__"))}
     return d

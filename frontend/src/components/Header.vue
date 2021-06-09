3812889bdc6632125fbb032aef2e00bb934d5191
Daniel Varoli <daniel.varoli@gmail.com>
2021-06-08 22:14:00 -0400

    basic upload component frontend

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-navbar>
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        Molo
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 

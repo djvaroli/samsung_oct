60d5552e7537ecce14fb201b8f5d6938a884dc85
Daniel Varoli <daniel.varoli@gmail.com>
2021-06-08 22:14:00 -0400

    basic upload component frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <Header></Header>
+    <ImageUploadComponent></ImageUploadComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
+import ImageUploadComponent from "./components/ImageUploadComponent";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImageUploadComponent,
+    Header
   }
 }
 </script>
---
+++ frontend/src/components/ImageUploadComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <vue-dropzone
+      ref="myVueDropzone"
+      id="oct-image-upload"
+      :options="dropzoneOptions"
+  >
+  </vue-dropzone>
+</template>
+
+<script>
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+
+export default {
+  name: "ImageUploadComponent",
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  data () {
+    return {
+      dropzoneOptions: {
+        url: 'https://httpbin.org/post',
+        thumbnailWidth: 150,
+        maxFilesize: 0.5,
+        headers: { "My-Awesome-Header": "header value" }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  #oct-image-upload {
+    border: 2px dashed #0087F7;
+  }
+
+</style>
